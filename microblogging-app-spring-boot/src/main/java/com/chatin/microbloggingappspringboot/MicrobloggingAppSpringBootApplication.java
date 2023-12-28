0a256aab3e5491f33c90a70e92e11205d84411d9
lukas.smicys <lukas.smicys@ext.fmc-ag.com>
2023-12-28 07:25:08 +0200

    Evironment ready

---
+++ microblogging-app-spring-boot/src/main/java/com/chatin/microbloggingappspringboot/MicrobloggingAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.chatin.microbloggingappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobloggingAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobloggingAppSpringBootApplication.class, args);
+	}
+
+}
